e8ef396cada73d1f2b2de7fdca3c4cf70371d426
jesusdesigner <jesuspunck@hotmail.com>
2025-08-28 15:38:18 -0300

    2
    2

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -4,54 +4,13 @@ import Logo from "./logo";
 export default function Footer({ border = false }: { border?: boolean }) {
   return (
     <footer>
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+      <div className="mx-auto max-w-6xl align- center px-4 sm:px-6">
         {/* Top area: Blocks */}
         <div
           className={`grid gap-10 py-8 sm:grid-cols-12 md:py-12 ${border ? "border-t [border-image:linear-gradient(to_right,transparent,var(--color-slate-200),transparent)1]" : ""}`}
         >
-          {/* 1st block */}
-          <div className="space-y-2 sm:col-span-12 lg:col-span-4">
-            <div>
-              <Logo />
-            </div>
-            <div className="text-sm text-gray-600">
-              &copy; FAVELA TECNOLOGIES - All rights reserved.
-            </div>
-          </div>
-
-          {/* 2nd block */}
-          <div className="space-y-2 sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h3 className="text-sm font-medium">Product</h3>
-            <ul className="space-y-2 text-sm">
-              <li>
-                <Link
-                  className="text-gray-600 transition hover:text-gray-900"
-                  href="#0"
-                >
-                  Features
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-gray-600 transition hover:text-gray-900"
-                  href="#0"
-                >
-                  Integrations
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-gray-600 transition hover:text-gray-900"
-                  href="#0"
-                >
-                  Pricing & Plans
-                </Link>
-              </li>
-                            
-            </ul>
-          </div>
-
-          {/* 3rd block */}
+          
+          
           
           {/* 5th block */}
           <div className="space-y-2 sm:col-span-6 md:col-span-3 lg:col-span-2">
@@ -104,7 +63,19 @@ export default function Footer({ border = false }: { border?: boolean }) {
               </li>
             </ul>
           </div>
+          
         </div>
+        {/* 1st block */}
+          <div className="space-y-2 sm:col-span-12 lg:col-span-4">
+            <div>
+              <Logo />
+            </div>
+            <div className="text-sm text-gray-600">
+              &copy; FAVELA TECNOLOGIES - All rights reserved.
+            </div>
+          </div>
+
+          
       </div>
 
       {/* Big text */}
